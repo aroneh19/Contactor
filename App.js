c36db7ada25ecda38291b5edc9936f9ace71a8be
Solvi2002 <151065525+Solvi2002@users.noreply.github.com>
2024-12-04 17:56:01 +0000

    Save progress before pulling from master

--- App.js
+++ App.js
@@ -5,23 +5,31 @@ import ContactView from "./src/views/ContactView";
 import ContactDetailView from "./src/views/ContactDetailView";
 import { ContactProvider } from "./src/Context/AppContext";
 
-
 const Stack = createStackNavigator();
 
 const App = () => {
 	return (
 		<ContactProvider>
-		<NavigationContainer>
-			<Stack.Navigator
-				initialRouteName="ContactList"
-				screenOptions={{
-					headerStyle: { backgroundColor: "#1a1a1a" },
-					headerTintColor: "#fff",
-				}}>
-				<Stack.Screen name="ContactList" component={ContactView} />
-				<Stack.Screen name="ContactDetailView" component={ContactDetailView} />
-			</Stack.Navigator>
-		</NavigationContainer>
+			<NavigationContainer>
+				<Stack.Navigator
+					initialRouteName="ContactList"
+					screenOptions={{
+						headerStyle: { backgroundColor: "#1a1a1a" },
+						headerTintColor: "#fff",
+					}}
+				>
+					<Stack.Screen
+						name="ContactList"
+						component={ContactView}
+						options={{ headerShown: false }}
+					/>
+					<Stack.Screen
+						name="ContactDetailView"
+						component={ContactDetailView}
+						options={{ title: "" }}
+					/>
+				</Stack.Navigator>
+			</NavigationContainer>
 		</ContactProvider>
 	);
 };
